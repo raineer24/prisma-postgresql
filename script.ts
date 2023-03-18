e9b29b79323e0a08bc56864b66fc1a1b2ad0c1d6
raineer <delaritaraineer81@gmail.com>
2023-03-18 13:30:34 +0800

    script ts

--- script.ts
+++ script.ts
@@ -2,26 +2,27 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.deleteMany()
-  const user = await prisma.user.create({
-    data: {
-      name: "Kyle",
-      email: "kyle@test.com",
-      age: 27,
-      userPreference: {
-        create: {
-          emailUpdates: true,
-        },
-      },
-    },
-    include: {
-      userPreference: true
-    }
+
+  // await prisma.user.createMany({
+  //   data: [
+  //     {
+  //       name: 'Sally',
+  //       age: 10,
+  //       email: 'sally1251@gmail.com'
+  //     }
+  //   ]
+  // })
+  
+  const user = await prisma.user.findMany({
+   where: {
+    name: "Sally",
+    age: { lt: 20}
+   },
+   
   });
 
   console.log(user);
 }
-
 
 main()
   .catch((e) => {
