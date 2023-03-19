1119165135d03d8b53c2e6f0734b240c3d76d480
raineer <delaritaraineer81@gmail.com>
2023-03-19 20:44:21 +0800

    signup auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
-
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'prisma/prisma.service';
 @Injectable()
 export class AuthService {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
     const { email, password } = dto;
-    return { message: 'signup was successfull' };
+
+    const foundUser = await this.prisma.user.findUnique({ where: { email } });
+
+    if (foundUser) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const hashedPassword = await this.hashPassword(password);
+
+    await this.prisma.user.create({
+      data: {
+        email,
+        hashedPassword,
+      },
+    });
+
+    return { message: 'User created successfully' };
   }
 
   async signin() {
     return '';
+  }
+
+  async hashPassword(password: string) {
+    const saltOrRounds = 10;
+
+    return await bcrypt.hash(password, saltOrRounds);
   }
 }
