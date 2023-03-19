f37a988202d66b87fc241e8e42c81bb30cac7a0f
raineer <delaritaraineer81@gmail.com>
2023-03-19 19:18:51 +0800

    auth dto signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
 
-  async signup() {
+  async signup(dto: AuthDto) {
+    const { email, password } = dto;
     return { message: 'signup was successfull' };
   }
 
