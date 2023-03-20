65aec5064858dd8bca67958a818cb7d17dc4d9ca
raineer <delaritaraineer81@gmail.com>
2023-03-20 15:55:36 +0800

    jwtauthguard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   getMyUser(@Param() params: { id: string }) {
     return;
