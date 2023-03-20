9e362f10353b19fc16f4a032a4f6500631ba508e
raineer <delaritaraineer81@gmail.com>
2023-03-20 15:15:10 +0800

    users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get(':id')
+  getMyUser(@Param() params: { id: string }) {
+    return;
+  }
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async getMyUser(id: string) {
+    return;
+  }
+
+  async getUsers() {
+    return await this.prisma.user.findMany();
+  }
+}
