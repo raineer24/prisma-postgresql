c1465015509332cf0c912d7b98edc5d5e2a2ee97
raineer <delaritaraineer81@gmail.com>
2023-03-22 19:43:41 +0800

    main ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('api');
   app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
