9d5dacdb17a23e6292cbb9b8567011f78a74340c
raineer <delaritaraineer81@gmail.com>
2023-03-22 11:11:24 +0800

    auth jwt strategy path

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { JwtStrategy } from 'src/auth/jwt.strategy';
-import { PrismaService } from '../prisma.service';
+import { JwtStrategy } from '../auth/jwt.strategy';
+
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, JwtStrategy, PrismaService],
+  providers: [UsersService, JwtStrategy],
 })
 export class UsersModule {}
