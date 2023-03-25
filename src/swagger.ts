76b481d1215e63012404ac33ae8377f2c62be877
raineer <delaritaraineer81@gmail.com>
2023-03-25 16:42:35 +0800

    swagger ts

---
+++ src/swagger.ts
@@ -0,0 +1,32 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { INestApplication } from '@nestjs/common';
+
+export default class SwaggerDocumentation {
+  private app: INestApplication;
+
+  constructor(app: INestApplication) {
+    this.app = app;
+  }
+
+  public serve(): void {
+    // Configure swagger
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('NestJS starter-kit')
+      .setDescription(
+        'NestJS web-server starter kit API, including User management and Role based authorizations',
+      )
+      .setContact('raineer', '', 'raineer@gmail.com')
+      .setVersion('1.0')
+      .addTag('Users')
+      .addTag('Authentication')
+      .addBearerAuth()
+      .build();
+
+    const swaggerDocument = SwaggerModule.createDocument(
+      this.app,
+      swaggerConfig,
+    );
+    SwaggerModule.setup('api', this.app, swaggerDocument);
+  }
+}
