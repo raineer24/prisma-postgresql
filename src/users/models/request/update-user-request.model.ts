d75bea38871479ba6e231f3e694e1cb7131878fe
raineer <delaritaraineer81@gmail.com>
2023-03-25 12:41:21 +0800

    update request

---
+++ src/users/models/request/update-user-request.model.ts
@@ -0,0 +1,25 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsUrl,
+  Matches,
+  MaxLength,
+} from 'class-validator';
+
+export class UpdateUserRequest {
+  @IsOptional()
+  @IsNotEmpty()
+  @Matches(RegExp('^[A-Za-zıöüçğşİÖÜÇĞŞñÑáéíóúÁÉÍÓÚ]+$'))
+  @MaxLength(20)
+  firstName?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @Matches(RegExp('^[A-Za-zıöüçğşİÖÜÇĞŞñÑáéíóúÁÉÍÓÚ ]+$'))
+  @MaxLength(40)
+  lastName?: string;
+
+  @IsOptional()
+  @IsUrl()
+  image?: string;
+}
