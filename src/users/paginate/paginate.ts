33638ed06b3079e033222d55ebabaf32d503bd4d
raineer <delaritaraineer81@gmail.com>
2023-03-26 19:33:32 +0800

    foreach delete hashedPassword

--- src/users/paginate/paginate.ts
+++ src/users/paginate/paginate.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
-
+import { Observable, from, throwError } from 'rxjs';
+import { switchMap, map, catchError } from 'rxjs/operators';
 @Injectable()
 export class Paginate {
   constructor(private readonly prisma: PrismaService) {}
@@ -9,11 +10,17 @@ export class Paginate {
     const results = await this.prisma.user.findMany({
       skip: page * size,
       take: Number(size),
+
       where: { firstName: { contains: search } },
       //   include: {
       //     lastName: true;
       //   },
     });
+
+    results.forEach(function (v) {
+      delete v.hashedPassword;
+    });
+    console.log('results', results);
     const totalItems = await this.prisma.user.count({
       where: { lastName: { contains: search, mode: 'insensitive' } },
     });
