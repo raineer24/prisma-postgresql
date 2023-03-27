25f50658670757ef3a58c5d7bb2ebb6ff63b89fc
raineer <delaritaraineer81@gmail.com>
2023-03-27 15:37:17 +0800

    new added column username

--- src/auth/models/request/signup.request.ts
+++ src/auth/models/request/signup.request.ts
@@ -24,4 +24,8 @@ export class SignupRequest {
   @Matches(RegExp('^[A-Za-zıöüçğşİÖÜÇĞŞñÑáéíóúÁÉÍÓÚ ]+$'))
   @MaxLength(20)
   lastName: string;
+
+  @IsNotEmpty()
+  @Matches(RegExp('^[A-Za-zıöüçğşİÖÜÇĞŞñÑáéíóúÁÉÍÓÚ ]+$'))
+  username: string;
 }
