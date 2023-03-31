d0502ca08eb0ea06cfa7f473203818e559958c85
raineer <delaritaraineer81@gmail.com>
2023-03-31 18:07:43 +0800

    prisma table chekieditedimage ;

---
+++ prisma/migrations/20230331100639_img/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "chekiEditedimage" (
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "chekiEditedimage_pkey" PRIMARY KEY ("id")
+);
