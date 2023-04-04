75764d16bf6f0ab5cf2960fe16b034a11e66378c
raineer <delaritaraineer81@gmail.com>
2023-04-04 14:49:58 +0800

    refresh token key

--- src/auth/shares/shares.service.ts
+++ src/auth/shares/shares.service.ts
@@ -18,15 +18,40 @@ export class SharesService {
   /*************************************************
    *  Encoding data
    */
-
+  async hashData(data: string): Promise<string> {
+    return await bcrypt.hash(data, 10);
+  }
   /*************************************************
    *  Compared data
    */
-
+  async compareData(data: string, hashData: string): Promise<boolean> {
+    return await bcrypt.compare(data, hashData);
+  }
   /*************************************************
    *  Generated a access_token and refresh_token
    */
+  async getTokens(userId: number, email: string): Promise<ITokens> {
+    const payload: ITokenPayload = {
+      sub: userId,
+      email,
+    };
 
+    const [access_token, refresh_token] = await Promise.all([
+      this.jwtService.sign(payload, {
+        secret: this.configService.get<string>('ACCESS_TOKEN_KEY'),
+        expiresIn: '7d',
+      }),
+      this.jwtService.sign(payload, {
+        secret: this.configService.get<string>('REFRESH_TOKEN_KEY'),
+        expiresIn: '7d',
+      }),
+    ]);
+
+    return {
+      access_token,
+      refresh_token,
+    };
+  }
   /*************************************************
    *  Update new refresh token to user
    */
