abc32b4f3e9268cb683e2fe642c77f1f563a0f95
raineer <delaritaraineer81@gmail.com>
2023-12-22 00:04:41 +0800

    paginate

--- src/blog/paginate/paginate.ts
+++ src/blog/paginate/paginate.ts
@@ -2,4 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class Paginate {}
+export class Paginate {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async pages(page, size, search) {
+    const results = await this.prisma.post.findMany({
+      skip: page * size,
+      take: Number(size),
+
+      where: { title: { contains: search } },
+    });
+
+    console.log('results', results);
+    const totalItems = await this.prisma.post.count({
+      where: { title: { contains: search, mode: 'insensitive' } },
+    });
+    return {
+      results,
+      totalItems,
+    };
+  }
+}
